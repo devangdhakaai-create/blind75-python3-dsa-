7679294c26bcda5eb8d739a0e6a6574d770fd94f
Devang Dhaka <devangdhaka.ai@gmail.com>
2026-05-15 09:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (40.50%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        
+        prev2 = 1
+        prev1 = 1
+        
+        for i in range(1, len(s)):
+            curr = 0
+            
+            if s[i] != '0':
+                curr += prev1
+            
+            two_digit = int(s[i-1:i+1])
+            if 10 <= two_digit <= 26:
+                curr += prev2
+            
+            prev2 = prev1
+            prev1 = curr
+        
+        return prev1
+# Agar encoding karni ho integer to str
+# def numEncodings(n: int) -> str:
+#     mapping = {i: chr(64 + i) for i in range(1, 27)}
+#     # {1:'A', 2:'B', ... 26:'Z'}
+    
+#     result = ""
+#     for ch in str(n):
+#         result += mapping[int(ch)]
+    
+#     return result
